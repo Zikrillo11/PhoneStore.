929d48bb0583c4412617c8f767a58bc92c6eee1e
Zikirillo11 <zikrilloyoldashev11@gmail.com>
2026-01-07 12:51:35 +0500

    fc

--- PhoneStore/Controllers/AccountController.cs
+++ PhoneStore/Controllers/AccountController.cs
@@ -15,7 +15,7 @@ public class AccountController : Controller
     public async Task<IActionResult> Login(string username, string password)
     {
         // vaqtinchalik admin
-        if (username == "fc" && password == "010203")
+        if (username == "admin" && password == "010203")
         {
             var claims = new List<Claim>
             {
